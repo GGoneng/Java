d59e1e29b98c3d06a6d033ecce6b0d9f134ae782
GGoneng <147640150+GGoneng@users.noreply.github.com>
2024-02-26 21:55:09 +0900

    1

--- src/CastingEx1.java
+++ src/CastingEx1.java
@@ -1,2 +1,9 @@
 public class CastingEx1 {
+    public static void main(String[] args) {
+        double d = 85.4;
+        int score = (int)d;
+
+        System.out.println("Score = " + score);
+        System.out.println("Double = " + d);
+    }
 }
